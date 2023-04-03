b997a63c9cedf8fa859fce7cdcab0cd7fd91ea22
Bryan Yan <bryanyan.tx@gmail.com>
2023-04-03 10:02:40 -0500

    added additional items to employeeview

--- my-app/src/components/pages/EmployeeView.js
+++ my-app/src/components/pages/EmployeeView.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import Card from '../Cards';
 import Footer from '../Footer';
+import CardList from '../CardList';
+
+const cardData = {
+    cards: [
+      {
+        image: "path/to/image1",
+        text: "testing",
+        label: "Label 1",
+        path: "/card1",
+      },
+      {
+        image: "path/to/image2",
+        text: "testing",
+        label: "Label 2",
+        path: "/card2",
+      },
+      {
+        image: "path/to/image1",
+        text: "Burger 1",
+        label: "Label 1",
+        path: "/card1",
+      },
+      {
+        image: "path/to/image1",
+        text: "Burger 1",
+        label: "Label 1",
+        path: "/card1",
+      },
+      {
+        image: "path/to/image1",
+        text: "Burger 1",
+        label: "Label 1",
+        path: "/card1",
+      },
+    ],
+  };
+
 function EmployeeView() {
     return (<>
+        <div id="employeemenu">
+            <CardList cardData={cardData} title="MENU" />
+        </div>
         <Footer/>
       </>);
 }
