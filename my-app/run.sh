ac91f3b6a9e206ceec4f2311133e1866f0cba9a5
Arjun Kurkal <arjunkurkal@tamu.edu>
2023-04-03 21:08:16 -0500

    added run.sh

---
+++ my-app/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ $OSTYPE == "darwin"* ]]; then
+  # macOS Terminal
+  node ./src/components/pages/database.js & npm start
+else
+  # PowerShell or Bash
+  node ./src/components/pages/database.js
+  npm start
+fi
