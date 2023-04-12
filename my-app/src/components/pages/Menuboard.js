8d1dda6c845527fb5cb965f19688029f68eb7a38
Daniela Santos <99611032+danielasantos21@users.noreply.github.com>
2023-04-12 15:50:17 -0500

    data from database into menuboard

--- my-app/src/components/pages/Menuboard.js
+++ my-app/src/components/pages/Menuboard.js
@@ -9,14 +9,12 @@ import Loading from '../Loading';
 */ }
 
 const MenuBoard = () => {
-  var menuItems = GetMenuList(1, 20);
-  var ingredientsArr = GetIngredients(1, 20);
+  var menuItems = GetMenuList(1, 26);
+  const ingredientsToHide = ["Tray Paper", "Black Bean Patty", "Straws", "Cups", "Fountain Drink Syrup", "Drink Lids"];
+  var ingredientsArr = GetIngredients(1, 26);
+  // var ingredientsArr = GetIngredients(1, 26);
 
   if (menuItems.length === 0 | ingredientsArr.length === 0) {
-    return <Loading />;
-  }
-
-  if (menuItems.length === 0 || ingredientsArr.length === 0) {
     return <Loading />;
   }
   
@@ -25,48 +23,60 @@ const MenuBoard = () => {
     const item = menuItems[i];
     const card = {
       name: item.menu_item_name,
-      price: 5.99,
-      ingredients: ingredientsArr[i]
+      price: item.menu_item_cost,
+      ingredients: ingredientsArr[i].filter((ingredient) => !ingredientsToHide.includes(ingredient))
     };
     burgersArr.push(card);
   }
   
   const basketsArr = [];
-  for (let i = 4; i < 7; i++) {
+  for (let i = 4; i < 6; i++) {
     const item = menuItems[i];
     const card = {
       name: item.menu_item_name,
-      price: 5.99,
-      ingredients: ingredientsArr[i]
+      price: item.menu_item_cost,
+      ingredients: ingredientsArr[i].filter((ingredient) => !ingredientsToHide.includes(ingredient))
     };
     basketsArr.push(card);
   }
 
   const sandwichesArr = [];
-  for (let i = 8; i < 12; i++) {
+  for (let i = 7; i < 12; i++) {
     const item = menuItems[i];
     const card = {
       name: item.menu_item_name,
-      price: 5.99,
-      ingredients: ingredientsArr[i]
+      price: item.menu_item_cost,
+      ingredients: ingredientsArr[i].filter((ingredient) => !ingredientsToHide.includes(ingredient))
     };
     sandwichesArr.push(card);
   }
 
   const sidesArr = [];
-  for (let i = 12; i < 20; i++) {
+  for (let i = 12; i < 25; i++) {
     const item = menuItems[i];
     const card = {
       name: item.menu_item_name,
-      price: 5.99,
-      ingredients: ingredientsArr[i]
+      price: item.menu_item_cost,
+      ingredients: []
     };
     sidesArr.push(card);
   }
 
+  //silverware is 25 but seasonal is not working
+  const seasonalArr = [];
+  for (let i = 25; i < 25; i++) {
+    const item = menuItems[i];
+    const card = {
+      name: item.menu_item_name,
+      price: item.menu_item_cost,
+      ingredients: []
+    };
+    seasonalArr.push(card);
+  }
+
   {/* HTML structure*/ }
   return (
-    <View burgers={burgersArr} baskets={basketsArr} sandwiches={sandwichesArr} sides = {sidesArr} seasonal= {[]} />
+    <View burgers={burgersArr} baskets={basketsArr} sandwiches={sandwichesArr} sides = {sidesArr} seasonal= {seasonalArr} />
   );
 };
 
