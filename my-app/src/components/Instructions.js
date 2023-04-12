cafc178efef3ffa0ad5cf6a229996c4050779769
Ariela Mitrani <amitrani@tamu.edu>
2023-04-12 15:48:07 -0500

    new tutorial sect

---
+++ my-app/src/components/Instructions.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Instructions.css';
+
+const Instructions = () => {
+  return (
+    <div className='instructions__container'>
+        <div className='title'>
+          How To Use This Website
+        </div>
+        <div className='paragraph'>
+          more cool info
+        </div>
+        <div className='paragraph'>
+          more cool info
+        </div>
+    </div>
+  );
+};
+
+export default Instructions;
+
--- my-app/src/components/Loading.js
+++ my-app/src/components/Loading.js
@@ -12,4 +12,3 @@ const Loading = () => {
 };
 
 export default Loading;
-
