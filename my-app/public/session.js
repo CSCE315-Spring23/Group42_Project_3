0b2e29461a6f41d6ff70342859efc3423bcb0109
Ariela Mitrani <amitrani@tamu.edu>
2023-04-13 00:09:00 -0500

    umique cookie per session generated

--- my-app/public/session.js
+++ my-app/public/session.js
@@ -1,6 +1,6 @@
-//const uuid = require('uuid').v4;
+const randomString = 'user_' + Math.random().toString(36).substr(2, 9);
 //console.log("cookie setting");
 //var sessionId = uuid();
 //localStorage.setItem('sessionId', sessionId);
-document.cookie = "sessionId=123adfa4";
+document.cookie = "sessionId=" + randomString;
 //console.log("cookie: ", sessionId);
