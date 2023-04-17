ac046a3c5341e3995f1502a8d148621d9e6501cd
Bryan Yan <bryanyan.tx@gmail.com>
2023-04-17 01:20:23 -0500

    added login to google button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,10 +15,32 @@ import MenuBoard from './components/pages/Menuboard';
 import ManagerView from './components/pages/ManagerView';
 import ManagerDatabaseAccess  from './components/pages/ManagerDatabaseAccess';
 
+import jwt_decode from "jwt-decode";
+import {useEffect} from 'react';
+
 //Main App documentation with all the different pages available
 function App() {
+  function handleCallbackResponse(response) {
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+  }
+
+  useEffect(()=> {
+    /* global google */
+    google.accounts.id.initialize({client_id: "351186300287-tmd28ore2cgjrc3j3mjcmrr29mel2ssa.apps.googleusercontent.com",
+    callback: handleCallbackResponse});
+
+    google.accounts.id.renderButton(
+      document.getElementById("signIn"),
+      {theme: "outline", size: "large"}
+    );
+  }, []);
+
   return (
     <>
+      <div className = "App">
+        <div id="signIn"></div>
+      </div>
       <Router>
         <Routes>
           <Route exact path='/' element={<Home/>} />
