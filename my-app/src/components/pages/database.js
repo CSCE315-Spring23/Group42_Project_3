e79232f90a6e2f84c66613f380747d4480b88617
SrikarPotlapalli <srikarpotlapallis@gmail.com>
2023-04-21 09:57:32 -0500

    added functionality to orders

--- my-app/src/components/pages/database.js
+++ my-app/src/components/pages/database.js
@@ -278,12 +278,14 @@ app.get('/createOrder/:menuItems/:ingredientList/:cost', async (req, res) => {
       let newItemID = parseInt(await pool.query('SELECT MAX(item_id) FROM item_sold'));
       let insertIntoItemSold = await pool.query("INSERT INTO item_sold (item_id, menu_item_id, order_id, item_sold_quantity) VALUES ('$1', '$2', '$3', '$4')", [newItemID, MenuId, newOrderID, quantity]);
       let updateMenu = await pool.query("UPDATE menu SET menu_item_sold_since_z = menu_item_sold_since_z + 1 WHERE menu_item_id= '$1'", [MenuId]);
-      let inventoryItemsForMenuItems = await pool.query("SELECT * FROM recipe_item WHERE menu_id = %d", MenuId);
+      let inventoryItemsForMenuItems = await pool.query("SELECT * FROM recipe_item WHERE menu_id = $1", [MenuId]);
       //  check getInventoryItemsForMenu if this doesnt work
-      const inventoryItemsForMenuItemsArray = inventoryItemsForMenuItems.rows.map((item) => item.inventory_item_name);
+      const amt_used = inventoryItemsForMenuItems.rows.map((item) => item.amt_used);
+      const inventory_id = inventoryItemsForMenuItems.rows.map((item) => item.inventory_id);
 
+      //update inventory item by adding a menu item.
       for (let j = 0; j < inventoryItemsForMenuItemsArray.length; j++){
-        let updateInventoryItem = await pool.query("UPDATE inventory_item SET inventory_item_quantity = inventory_item_quantity - $1 WHERE inventory_id = $2", [inventoryItemsForMenuItemsArray.get(i).amt_used, vector2.get(i)]);
+        let updateInventoryItem = await pool.query("UPDATE inventory_item SET inventory_item_quantity = inventory_item_quantity - $1 WHERE inventory_id = $2", [amt_used.get(i), inventory_id.get(i)]);
       }
 
     }
--- my-app/src/components/pages/databaseFunctions.js
+++ my-app/src/components/pages/databaseFunctions.js
@@ -179,126 +179,126 @@ function GetCartItems(){
   return items;
 }
 
-// function createOrder(type, name, quantity, price) {
-//   const [myCart, setCart] = useState([]);
-//   var myID = document.cookie.replace(/(?:(?:^|.*;\s*)sessionId\s*=\s*([^;]*).*$)|^.*$/, "$1");
-//   //console.log("getting cart");
+function createOrder(type, name, quantity, price) {
+  const [myCart, setCart] = useState([]);
+  var myID = document.cookie.replace(/(?:(?:^|.*;\s*)sessionId\s*=\s*([^;]*).*$)|^.*$/, "$1");
+  //console.log("getting cart");
 
-//   useEffect(() => {
-//     async function fetchCart() {
-//       const response = await fetch(`http://localhost:3001/getCart/${myID}`);
-//       const data = await response.json();
-//       setCart(data.rows[0].orderlist);
-//       //console.log("cart in func: " + data.rows[0].orderlist);
-//     }
-//     fetchCart();
-//   }, [myID]);
+  useEffect(() => {
+    async function fetchCart() {
+      const response = await fetch(`http://localhost:3001/getCart/${myID}`);
+      const data = await response.json();
+      setCart(data.rows[0].orderlist);
+      //console.log("cart in func: " + data.rows[0].orderlist);
+    }
+    fetchCart();
+  }, [myID]);
 
-//   //console.log("cart: " + myCart); // should log an array of cart items
-//   const menuItems = [];
-//   const ingredientList = [];
-//   var j = 1;
-//   let cost = 0;
+  //console.log("cart: " + myCart); // should log an array of cart items
+  const menuItems = [];
+  const ingredientList = [];
+  var j = 1;
+  let cost = 0;
 
-//   if(myCart !== null) {
-//     for (let i = 0; i < myCart.length; i++) {
-//       const element = JSON.parse(myCart[i]);
-//       let pair = { first: element.id, second: element.qty};
-//       if(element.type === "item") {
-//         cost += item.price;
-//         menuItems.push(pair);
-//         ingredientList.length = 0;
-//         j++;
-//       } 
-//       else {
-//           if(element.name === "Beef Patty") {
-//             if(element.quantity === -1){
-//               ingredientList.push(pair);//add beef patty with -1 quantity
-//               pair.first = 3;
-//               pair.second = 1;
-//               ingredientList.push(pair);//add black bean patty
-//             }
-//           }
-//           else if(element.name === "Vanilla Ice Cream") {
-//             if(element.quantity === -1){
-//               pair.first = 18;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a chocolate ice cream
-//             }
-//             else if(element.quantity === 0){
-//               pair.first = 19;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a vanilla ice cream
-//             }
-//             else if(element.quantity === 1){
-//               pair.first = 21;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a coffee ice cream
-//             }
-//             else{
-//               pair.first = 20;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a strawberru ice cream
-//             }
-//           }
-//           else if(element.name === "x") {
-//             if(element.quantity === -1){
-//               pair.first = 24;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a buffalo sauce
-//             }
-//             else if(element.quantity === 0){
-//               pair.first = 25;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a BBQ sauce
-//             }
-//             else if(element.quantity === 1){
-//               pair.first = 26;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a Honey mustard sauce
-//             }
-//             else if(element.quantity === 2){
-//               pair.first = 27;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a ranch sauce
-//             }
-//             else if(element.quantity === 3){
-//               pair.first = 28;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a spicy ranch sauce
-//             }
-//             else if(element.quantity === 4){
-//               pair.first = 4;
-//               pair.second = 1;
-//               ingredientList.push(pair); //add a gig em sauce
-//             }
-//           }
-//           else {
-//             if(element.quantity === -1){
-//               ingredientList.push(pair); //push item with quanity -1
-//             }
-//             else{
-//               cost += element.price;
-//               ingredientList.push(pair); //push item with given quanity
-//             }
-//           }
-//         }
-//     };
+  if(myCart !== null) {
+    for (let i = 0; i < myCart.length; i++) {
+      const element = JSON.parse(myCart[i]);
+      let pair = { first: element.id, second: element.qty};
+      if(element.type === "item") {
+        cost += item.price;
+        menuItems.push(pair);
+        ingredientList.length = 0;
+        j++;
+      } 
+      else {
+          if(element.name === "Beef Patty") {
+            if(element.quantity === -1){
+              ingredientList.push(pair);//add beef patty with -1 quantity
+              pair.first = 3;
+              pair.second = 1;
+              ingredientList.push(pair);//add black bean patty
+            }
+          }
+          else if(element.name === "Vanilla Ice Cream") {
+            if(element.quantity === -1){
+              pair.first = 18;
+              pair.second = 1;
+              ingredientList.push(pair); //add a chocolate ice cream
+            }
+            else if(element.quantity === 0){
+              pair.first = 19;
+              pair.second = 1;
+              ingredientList.push(pair); //add a vanilla ice cream
+            }
+            else if(element.quantity === 1){
+              pair.first = 21;
+              pair.second = 1;
+              ingredientList.push(pair); //add a coffee ice cream
+            }
+            else{
+              pair.first = 20;
+              pair.second = 1;
+              ingredientList.push(pair); //add a strawberru ice cream
+            }
+          }
+          else if(element.name === "x") {
+            if(element.quantity === -1){
+              pair.first = 24;
+              pair.second = 1;
+              ingredientList.push(pair); //add a buffalo sauce
+            }
+            else if(element.quantity === 0){
+              pair.first = 25;
+              pair.second = 1;
+              ingredientList.push(pair); //add a BBQ sauce
+            }
+            else if(element.quantity === 1){
+              pair.first = 26;
+              pair.second = 1;
+              ingredientList.push(pair); //add a Honey mustard sauce
+            }
+            else if(element.quantity === 2){
+              pair.first = 27;
+              pair.second = 1;
+              ingredientList.push(pair); //add a ranch sauce
+            }
+            else if(element.quantity === 3){
+              pair.first = 28;
+              pair.second = 1;
+              ingredientList.push(pair); //add a spicy ranch sauce
+            }
+            else if(element.quantity === 4){
+              pair.first = 4;
+              pair.second = 1;
+              ingredientList.push(pair); //add a gig em sauce
+            }
+          }
+          else {
+            if(element.quantity === -1){
+              ingredientList.push(pair); //push item with quanity -1
+            }
+            else{
+              cost += element.price;
+              ingredientList.push(pair); //push item with given quanity
+            }
+          }
+        }
+    };
 
-//     createOrderPromise = createOrderPromise.then(async () => {
-//       const response = await fetch(`http://localhost:3001/createOrder/${menuItems}/${ingredientList}${cost}`, {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(item)
-//       });
-//       const data = await response.json();
-//       console.log(data.message);
-//     });
-//   }
-//   return items;
-// }
+    createOrderPromise = createOrderPromise.then(async () => {
+      const response = await fetch(`http://localhost:3001/createOrder/${menuItems}/${ingredientList}${cost}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+      });
+      const data = await response.json();
+      console.log(data.message);
+    });
+  }
+  return items;
+}
 
 
 export {GetMenuList, GetIngredients, AddToCart, GetCartItems, GetInventoryList};
