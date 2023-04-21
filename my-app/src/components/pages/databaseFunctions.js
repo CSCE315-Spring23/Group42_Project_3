7a12abd7d571981f0750e5f9da12d6a51eb3cc58
SrikarPotlapalli <srikarpotlapallis@gmail.com>
2023-04-20 23:06:13 -0500

    create order function added

--- my-app/src/components/pages/databaseFunctions.js
+++ my-app/src/components/pages/databaseFunctions.js
@@ -158,5 +158,87 @@ function GetCartItems(){
   return items;
 }
 
+function Checkout(type, name, quantity, price) {
+  const [myCart, setCart] = useState([]);
+  var myID = document.cookie.replace(/(?:(?:^|.*;\s*)sessionId\s*=\s*([^;]*).*$)|^.*$/, "$1");
+  //console.log("getting cart");
+
+  useEffect(() => {
+    async function fetchCart() {
+      const response = await fetch(`http://localhost:3001/getCart/${myID}`);
+      const data = await response.json();
+      setCart(data.rows[0].orderlist);
+      //console.log("cart in func: " + data.rows[0].orderlist);
+    }
+    fetchCart();
+  }, [myID]);
+
+  //console.log("cart: " + myCart); // should log an array of cart items
+  const items = [];
+  const modlist = [];
+  var j = 1;
+
+  if(myCart !== null) {
+    for (let i = 0; i < myCart.length; i++) {
+      const element = JSON.parse(myCart[i]);
+      if(element.type === "item") {
+        const item = { id: j, name: element.name, price: element.price, qty: 1, mods: [...modlist]}
+        items.push(item);
+        modlist.length = 0;
+        j++;
+      } 
+      else {
+          if(element.name === "Beef Patty") {
+            if(element.quantity === -1)
+              modlist.push("SUB Black Bean Patty");
+          }
+          else if(element.name === "Vanilla Ice Cream") {
+            if(element.quantity === -1)
+              modlist.push("CHOCOLATE");
+            else if(element.quantity === 0)
+              modlist.push("VANILLA");
+            else if(element.quantity === 1)
+              modlist.push("COFFEE");
+            else
+              modlist.push("STRAWBERRY");
+          }
+          else if(element.name === "x") {
+            if(element.quantity === -1)
+              modlist.push("BUFFALO");
+            else if(element.quantity === 0)
+              modlist.push("BBQ");
+            else if(element.quantity === 1)
+              modlist.push("HONEY MUSTARD");
+            else if(element.quantity === 2)
+              modlist.push("RANCH");
+            else if(element.quantity === 3)
+              modlist.push("SPICY RANCH");
+            else if(element.quantity === 4)
+              modlist.push("GIG EM");
+          }
+          else {
+            if(element.quantity === -1)
+              modlist.push("NO " + element.name);
+            else
+              modlist.push("EXTRA " + element.name);
+          }
+      }
+    };
+
+    addToCartPromise = addToCartPromise.then(async () => {
+      const response = await fetch(`http://localhost:3001/checkout/${myID}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+      });
+      const data = await response.json();
+      console.log(data.message);
+    });
+  }
+  return items;
+}
+
 
 export {GetMenuList, GetIngredients, AddToCart, GetCartItems, GetInventoryList};
