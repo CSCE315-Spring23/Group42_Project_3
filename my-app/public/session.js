5f0d650493df56c3c0170bb6bc749815188de6a6
Ariela Mitrani <amitrani@tamu.edu>
2023-04-24 15:22:46 -0500

    fixed cookies not initializing

--- my-app/public/session.js
+++ my-app/public/session.js
@@ -1,21 +1,22 @@
-var oldID = document.cookie.replace(/(?:(?:^|.*;\s*)sessionId\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-console.log("old cookie: " + oldID)
-EndSession(oldID);
-const randomString = 'user_' + Math.random().toString(36).substr(2, 9);
-document.cookie = "sessionId=" + randomString;
-console.log("new cookie: " + randomString);
-CreateSession(randomString);
 const isLocalhost = window.location.hostname === 'localhost'; // Check if current hostname is localhost
 const host = isLocalhost ? 'http://localhost:3001' : 'https://revs-american-grill-z267.onrender.com/'; // Set host based on current environment
 
-async function CreateSession(myID) {
-  const response = await fetch(`${host}/${myID}`);
+var oldID = document.cookie.replace(/(?:(?:^|.*;\s*)sessionId\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+console.log("old cookie: " + oldID);
+EndSession(oldID, host);
+const randomString = 'user_' + Math.random().toString(36).substr(2, 9);
+document.cookie = "sessionId=" + randomString;
+console.log("new cookie: " + randomString);
+CreateSession(randomString, host);
+
+async function CreateSession(myID, host) {
+  const response = await fetch(`${host}/checkSession/${myID}`);
   const data = await response.json();
   return data;
 }
 
-async function EndSession(myID) {
-  const response = await fetch(`${host}/${myID}`);
+async function EndSession(myID, host) {
+  const response = await fetch(`${host}/endSession/${myID}`);
   const data = await response.json();
   return data;
 }
