842eafd577dae94a6a0fc40844c7973d1ba3aa16
Ariela Mitrani <amitrani@tamu.edu>
2023-04-24 23:10:32 -0500

    i cannot work on this any longer

--- my-app/src/components/LoginButton.js
+++ my-app/src/components/LoginButton.js
@@ -2,41 +2,37 @@ import jwt_decode from "jwt-decode";
 import {useEffect, useState} from 'react';
 
 //Main App documentation with all the different pages available
-function LoginButton() {
+function LoginButton({onUserUpdate}) {
     const [user, setUser] = useState({});
     function handleCallbackResponse(response) {
+      //console.log("callback running");
       var userObject = jwt_decode(response.credential);
-      console.log(userObject);
       setUser(userObject);
-      document.getElementById("signIn").hidden = true;
+      //console.log("btn user data: " + JSON.stringify(userObject));
+      //document.getElementById("signIn").hidden = true;
+      onUserUpdate(userObject);
     }
-  
+
     useEffect(()=> {
       /* global google */
-      google.accounts.id.initialize({client_id: "351186300287-tmd28ore2cgjrc3j3mjcmrr29mel2ssa.apps.googleusercontent.com",
-      callback: handleCallbackResponse});
-  
+      google.accounts.id.initialize({client_id: "351186300287-tmd28ore2cgjrc3j3mjcmrr29mel2ssa.apps.googleusercontent.com", callback: handleCallbackResponse});
+
+      onUserUpdate(user);
+
+      //login and stuff
       google.accounts.id.renderButton(
         document.getElementById("signIn"),
         {theme: "outline", size: "large"}
       );
     }, []);
-  
+
     return (
       <>
         <div className = "App">
           <div id="signIn"></div>
-          { user && 
-          <div>
-            <img src={user.picture}></img>
-            <h3>{user.email}</h3>
-            
-            </div>
-          }
         </div>
         </>
     );
   }
-  
-  export default LoginButton;
-  
+
+  export {LoginButton};
