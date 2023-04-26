86c25ad076653037bdd6ab6dd6e482163dbf9401
Arjun Kurkal <arjunkurkal@tamu.edu>
2023-04-26 17:50:38 -0500

    reverting previous changes

--- my-app/src/components/pages/database.js
+++ my-app/src/components/pages/database.js
@@ -362,6 +362,6 @@ app.get('*', (req, res) => {
 
 
 // Start the server
-app.listen(isLocalhost ? 3001 : 10000, () => {
+app.listen(3001, () => {
   console.log('Server listening on port 3001');
 });
