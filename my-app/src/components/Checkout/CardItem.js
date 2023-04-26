a3707c78834041b608625399cc651826de553bf8
Daniela Santos <99611032+danielasantos21@users.noreply.github.com>
2023-04-26 18:43:40 -0500

    merge conflicts

--- my-app/src/components/Checkout/CardItem.js
+++ my-app/src/components/Checkout/CardItem.js
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Popup from '../Popup';
-import './cart.css'
-
-//Used for displaying menu items in our menu page
-function CardItem(props) {
-  const [showPopup, setShowPopup] = useState(false);
-
-  function togglePopup() {
-    setShowPopup(!showPopup);
-  }
-
-  return (
-    <>
-      <li className='ci'>
-        {props.path ? (
-          <Link className='ci__link' to={props.path}>
-            <figure className='ci__pic-wrap' data-category={props.label}>
-              <img
-                className='ci__img'
-                alt='Food item'
-                src={props.src}
-              />
-            </figure>
-            <div className='ci__info'>
-              <h5 className='ci__text'>{props.text}</h5>
-            </div>
-          </Link>
-        ) : (
-          <Link className='ci__link' onClick={togglePopup}>
-            <figure className='ci__pic-wrap' data-category={props.label}>
-              <img
-                className='ci__img'
-                alt='Food item'
-                src={props.src}
-              />
-            </figure>
-            <div className='ci__info'>
-              <h5 className='ci__text'>{props.text}</h5>
-            </div>
-          </Link>
-        )}
-      </li>
-      {showPopup && (
-        <Popup text = {props.text} price = {props.label} ingredients = {props.ingredients} onClose={togglePopup} />
-      )}
-    </>
-  );
-}
-
-export default CardItem;
-=======
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Popup from '../Popup';
@@ -104,4 +50,3 @@ function CardItem(props) {
 }
 
 export default CardItem;
->>>>>>> 64145927a16909d19fccbc86529cc0bd436ede8f
