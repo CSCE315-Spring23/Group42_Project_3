098e81db17a0edd8ff84dccbc1adc44f5e1943a6
Daniela Santos <99611032+danielasantos21@users.noreply.github.com>
2023-05-01 15:22:21 -0500

    Popup directory documentation

--- my-app/src/components/Popups/LoginButton.js
+++ my-app/src/components/Popups/LoginButton.js
@@ -2,9 +2,19 @@ import jwt_decode from "jwt-decode";
 import {useEffect, useState} from 'react';
 import "./Popup.css"
 
-//Main App documentation with all the different pages available
+/**
+  * Component for rendering a login button that uses Google Sign-In to authenticate users.
+  * @component
+  * @param {Function} onUserUpdate - A callback function that takes in the user object as a parameter and updates the user state in the parent component.
+  * @returns {JSX.Element} A login button component that uses Google Sign-In.
+*/
 function LoginButton({onUserUpdate}) {
+  
     const [user, setUser] = useState({});
+    /**
+    *  Callback function to handle the response from Google Sign-In. Decodes the JWT token returned by Google and updates the user state using the setUser function. Calls the onUserUpdate function to update the user state in the parent component.
+    * @param {Object} response - The response object returned by Google Sign-In.
+    */
     function handleCallbackResponse(response) {
       //console.log("callback running");
       var userObject = jwt_decode(response.credential);
--- my-app/src/components/Popups/Popup.js
+++ my-app/src/components/Popups/Popup.js
@@ -6,11 +6,26 @@ import {Login} from './Login'
 import { Link } from 'react-router-dom';
 import './Popup.css';
 
+/**
+  * Popup component that displays a pop-up window with ingredient form, add to cart button, and checkout button.
+  * @param {Object} props - The props that are passed to this component.
+  * @param {string} props.text - The text to be displayed in the popup window.
+  * @param {Array<string>} props.ingredients - The list of ingredients to be displayed in the ingredient form.
+  * @param {function} props.onClose - The function to be called when the popup window is closed.
+  * @param {number} props.price - The price of the item.
+  * @param {string} props.popupStyle - The style of the popup window.
+  * @returns {JSX.Element} - Returns a JSX element that displays the popup window.
+*/
+
 function Popup({ text, ingredients, onClose, price, popupStyle = 'style1'}) {
   const [quantities, setQuantities] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
   const ingredientsToHide = ["Tray Paper", "Black Bean Patty", "Buns", "Small Paper Containers", "Sauce Containers", "Strawberry Ice Cream", "Straws", "Cups", "Chocolate Ice Cream", "Coffee Ice Cream", "Drink Lids", "Milk", "Chocolate Chunk Cookie", "Chocolate Fudge Brownie", "Fountain Drink Syrup", "Coffee", "Fries", "Tater Tots", "Onion Rings", "Kettle Chips", "Chicken Tenders", "Steak Fingers", "Seasonal"];
   const visibleIngredients = ingredients ? ingredients.filter((ingredient) => !ingredientsToHide.includes(ingredient)) : [];
 
+  /**
+  * Function to add all ingredients to cart.
+  * @async
+  */
   async function addAllToCart() {
     for (let i = 0; i < visibleIngredients.length; i++) {
       if (quantities[i] !== 0 || visibleIngredients[i] === "Vanilla Ice Cream" || visibleIngredients[i] === "x" || visibleIngredients[i] === "Combo" || visibleIngredients[i] === "Basket Combo") {
@@ -20,6 +35,10 @@ function Popup({ text, ingredients, onClose, price, popupStyle = 'style1'}) {
     await AddToCart("item", text, 1, price);
   }
 
+  /**
+  * Function to handle checkout button click.
+  * @async
+  */
   async function checkoutClick() {
     await addAllToCart();
     localStorage.setItem("checkout", "true");
